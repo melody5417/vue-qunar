15e2d9060ca979214a4b28664cb1817d2f720ba8
yiqiwang <yiqiwang@tencent.com>
2020-02-01 14:04:12 +0800

    Add router

--- qunar/src/main.js
+++ qunar/src/main.js
@@ -1,8 +1,11 @@
 import Vue from 'vue'
 import App from './App.vue'
+import Router from './router/router.js'
 
 Vue.config.productionTip = false
 
 new Vue({
-  render: h => h(App),
+	// es6 键和值相同可以省略 "键名:" 直接写 值
+	router: Router,
+	render: h => h(App),
 }).$mount('#app')
