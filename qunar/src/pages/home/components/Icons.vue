56f262d521406e27d7ef37ba3f4a63c92e4e4717
yiqiwang <yiqiwang@tencent.com>
2020-02-02 19:13:23 +0800

    完成图标区域一个图标

--- qunar/src/pages/home/components/Icons.vue
+++ qunar/src/pages/home/components/Icons.vue
@@ -1,5 +1,12 @@
 <template>
-	<div> icons </div>
+	<div class="icons">
+		<div class="icon">
+			<div class="icon-img">
+				<img class="icon-img-content" src="https://img1.qunarzz.com/travel/poi/1804/f8/654d1576a7497d37.jpg" />
+			</div>
+			<p class="icon-desc">热门景点</p>
+		</div>
+	</div>
 </template>
 
 <script>
@@ -9,4 +16,42 @@
 </script>
 
 <style lang="stylus" scoped>
+	@import '../../../assets/styles/variables.styl'
+	.icons
+		overflow hidden
+		width 100%
+		height 0
+		padding-bottom 50%
+		background #42B983
+		.icon
+			position relative
+			overflow hidden
+			float left
+			width 25%
+			padding-bottom 25%
+			background #00BCD4
+			.icon-img
+				position absolute
+				top 0
+				left 0
+				bottom .44rem
+				right 0
+				box-sizing border-box
+				padding-bottom .1rem
+				.icon-img-content
+					display block
+					margin 0 auto
+					height 100%
+			.icon-desc
+				position absolute
+				left 0
+				right 0
+				bottom 0
+				height .44rem
+				line-height .44rem //文字上下居中
+				color $dartTextColor
+				text-align center
+				
+			
+			
 </style>
