d2ea234d405264517e2bd7f49b65294f730cc7c9
yiqiwang <yiqiwang@tencent.com>
2020-02-02 16:10:43 +0800

    Add swiper 添加轮播组件

--- qunar/src/main.js
+++ qunar/src/main.js
@@ -5,10 +5,14 @@ import fastClick from 'fastclick'
 import 'styles/reset.css'
 import	'styles/border.css'
 import	'styles/iconfont.css'
+import VueAwesomeSwiper from 'vue-awesome-swiper'
+import 'swiper/dist/css/swiper.css'
 
 Vue.config.productionTip = false
 // 解决浏览器click事件300ms延迟的问题
 fastClick.attach(document.body)
+// 轮播组件
+Vue.use(VueAwesomeSwiper)
 
 new Vue({
 	// es6 键和值相同可以省略 "键名:" 直接写 值
---
+++ qunar/src/pages/home/components/Swiper.vue
@@ -0,0 +1,55 @@
+<template>
+	<div class="wrapper">
+		<swiper :options="swiperOption">
+			<swiper-slide v-for="item of swiperList" :key="item.id">
+				<img class="swiper-mg" :src="item.imgUrl">
+			</swiper-slide>
+			<div class="swiper-pagination"  slot="pagination"></div>
+		</swiper>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'HomeSwiper',
+		data () {
+			return {
+				// Swiper4的API文档 https://www.swiper.com.cn/api/loop/22.html
+				swiperOption: {
+					// 如果需要分页器
+					pagination: {
+						el: '.swiper-pagination'
+					},
+					loop: true,
+					autoplay: true,
+					delay: 200
+				},
+				swiperList: [{
+					id: '0001',
+					imgUrl: 'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/316fb27c0e1ac2ee1996cbc5ec2d5b3f.jpg'
+				}, {
+					id: '0002',
+					imgUrl:	'https://tr-osdcp.qunarzz.com/tr-osd-tr-manager/img/99942c1e5b16f528723dd4c0fbf9c97c.jpg'
+				}]
+			}
+		}
+	}
+</script>
+
+<style lang="stylus" scoped>
+	// >>> 样式穿透
+	.wrapper >>> .swiper-pagination-bullet-active 
+		background: #fff
+	.wrapper
+		.swiper-container
+			width 100%
+			height 100%
+		overflow hidden
+		width 100%
+		height 31.25vw
+		background #CACACA
+		.swipe-img
+		// ？此处未充满 要怎么设置
+			width 100%
+
+</style>
