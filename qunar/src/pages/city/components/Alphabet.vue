c8f6d940b28fc7a1d4cb82e936327c057abee730
yiqiwang <yiqiwang@tencent.com>
2020-02-04 14:45:32 +0800

    添加字母表

---
+++ qunar/src/pages/city/components/Alphabet.vue
@@ -0,0 +1,35 @@
+<template>
+	<div class="list">
+		<ul>
+			<li class="item">A</li>
+			<li class="item">A</li>
+			<li class="item">A</li>
+			<li class="item">A</li>
+			<li class="item">A</li>
+			<li class="item">A</li>
+		</ul>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: "CityAlphabet"
+	}
+</script>
+
+<style lang="stylus" scoped>
+	.list
+		overflow hidden
+		position absolute
+		top 2.8rem
+		right 0
+		bottom 0
+		width .4rem
+		display flex
+		flex-direction column
+		justify-content center
+		.item
+			line-height .4rem
+			color #00BCD4
+		
+</style>
--- qunar/src/pages/city/components/List.vue
+++ qunar/src/pages/city/components/List.vue
@@ -127,6 +127,7 @@
 		right 0
 		bottom 0
 		left 0
+		background-color #FFFFFF
 		.title
 			line-height .44rem
 			background-color #eee
