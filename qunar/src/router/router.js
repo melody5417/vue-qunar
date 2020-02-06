d48321d6e22dca429da6559bd29c26a4901a7c47
yiqiwang <yiqiwang@tencent.com>
2020-02-06 13:24:34 +0800

    组件按需加载

--- qunar/src/router/router.js
+++ qunar/src/router/router.js
@@ -1,6 +1,5 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import Home from '../pages/home/Home.vue'
 import City from '../pages/city/City.vue'
 import Detail from '../pages/detail/Detail.vue'
 
@@ -11,7 +10,7 @@ export default new Router({
 		{
 			path: '/',
 			name: 'Home',
-			component: Home
+			component: () => ('../pages/home/Home.vue')
 		}, {
 			path: '/city',
 			name: 'City',
